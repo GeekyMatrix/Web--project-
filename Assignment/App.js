ac49edbfcb819e35024928a5ac6e712dfa40fe0b
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-11-20 21:10:15 +0530

    Create App.js

---
+++ Assignment/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Board from './src/KanbanBoard';
+
+const App = () => {
+   return (
+     
+
+       
+KanbanBoard
+
+       
+     
+
+   );
+};
+
+export default App;
